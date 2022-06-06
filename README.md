7f976cd1b4aa7145cc92316741bfe1d7cffa66d2
Andres <gato73127@hotmail.com>
2022-06-06 09:51:32 -0500

    Adding bob file and readme file update

--- README.md
+++ README.md
@@ -259,5 +259,21 @@ Starting with n = 12, the steps would be as follows:
 9.1
 Resulting in 9 steps. So for input n = 12, the return value would be 9.
 
+BOB
+Instructions
+Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+Bob answers 'Sure.' if you ask him a question, such as "How are you?".
+
+He answers 'Whoa, chill out!' if you YELL AT HIM (in all capitals).
+
+He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+
+He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+He answers 'Whatever.' to anything else.
+
+Bob's conversational partner is a purist when it comes to written communication and always follows normal rules regarding sentence punctuation in English.
+
 
 Andres Felipe
--- armstrong_numbers.py
+++ armstrong_numbers.py
@@ -1,15 +1,20 @@
 '''Program to determine if a number is an armstrong number or not'''
+import time
+time1 = 0
+time2 = 0
+time3 = 0
 def is_armstrong_number(number):
     x =  [int(a) for a in str(number)]
     n = len(x)
     c = []
-    for a in x:
+    return sum(a ** len(x) for a in x) == number
+    ''' for a in x:
        b = a**n
        c.append(b)
     if sum(c) == number:
         return True
     return False
-
+    '''
 def is_armstrong_number1(number):
     digits = 0
     a = number
@@ -24,7 +29,47 @@ def is_armstrong_number1(number):
     
     return suma == number
 
+def _get_digits(number: int) -> list[int]:
+    digits = []
+    while number:
+        digit = number % 10
+        digits.append(digit)
+        number //= 10
+    return digits
+
+def is_armstrong_number2(number):
+    digits = _get_digits(number)
+    return sum(a ** len(digits) for a in digits) == number
+
+def is_armstrong_number3(number):
+    digits = []
+    c = number
+    while c:
+        digit = c % 10
+        digits.append(digit)
+        c //= 10
+    return sum(a ** len(digits) for a in digits) == number
+
 if __name__=='__main__':
     number =  int(input('Please introduce a number\n'))
+    start = time.time()
     print(is_armstrong_number(number))
+    end = time.time()
+    start1= time.time()
     print(is_armstrong_number1(number))
+    end1 = time.time()
+    start2 = time.time()
+    print(is_armstrong_number2(number))
+    end2 = time.time()
+    start3 = time.time()
+    print(is_armstrong_number3(number))
+    end3 = time.time()
+
+time1 = end - start
+time2 = end1 - start1
+time3 = end2 - start2
+time4 = end3 - start3
+print('Algoritmo uno toma: ', time1)
+print('Algoritmo dos toma: ', time2)
+print('Algoritmo 3   toma: ', time3)
+print('Algoritmo 4   toma: ', time4)
