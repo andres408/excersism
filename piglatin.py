2a5d8b2913983cd5084ee53427e32dac1718a712
Kali <kali@kali>
2022-06-09 18:30:29 -0400

    Add piglatin file update and readme file update

--- piglatin.py
+++ piglatin.py
@@ -1,7 +1,11 @@
+'''Program to change words according to pig latin game rules.'''
 vowels=['a','e','i','o','u']
 vowelsa=['xr','yt']
 
 def words(word):
+    '''Param: str - word from translate function
+       Return : str - word with suffix according to game rules'''
+    b = 0
     vowelsound= word[0] in vowels or word[:2] in vowelsa
     cluster= word[:2] not in vowels and word[:2] not in vowelsa
     if vowelsound:
@@ -14,13 +18,21 @@ def words(word):
         return word.strip(word[:2]) + word[:2] + 'ay'
     if word[:2] =='qu':
         return word.strip(word[:2]) + word[:2] +'ay'
-    if word[:3] not in vowels:
-        return word.strip(word[:3]) + word[:3] + 'ay'
     if not vowelsound:
+        for a in word[:3]:
+            if a not in vowels:
+                b += 1
+            if b == 3:
+                return word[3:] + word[:3] +'ay'
         if word[1] not in vowels:
             return word.strip(word[:2]) + word[:2] + 'ay'
         return word.strip(word[0]) + word[0] + 'ay'
-        
+
+def translate(text):
+    '''Param: str -  word or words introduced by user
+        Return: str - the changed words in order according to words function.'''
+    return ' '.join(words(word) for word in text.split())
+
 if __name__=='__main__':
-    word = str(input('Ingresa palabra\n'))
-    print(words(word))
+    text = str(input('Ingresa palabra\n'))
+    print(translate(text))
