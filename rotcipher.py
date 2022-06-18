87b4646f1050d9cde00c096ce2e42f2110210010
Kali <kali@kali>
2022-06-18 15:25:03 -0400

    Add darts.py, rotcipher.py files and updates in readme, rotational_cipher.py files

--- rotcipher.py
+++ rotcipher.py
@@ -1,15 +1,11 @@
 import string
 a = (string.ascii_lowercase*2)
 u = (string.ascii_uppercase*5)
-d = ('1234567890'*5)
-p = ('<>,.-_:;/&%$#\"\'?¿=*+-!' * 5)
 b = []
 def rot(text,key):
     for i in text:
-        if i in p:
-            b.append(p[p.index(i) + key])
-        if i in string.digits:
-            b.append(d[d.index(i) + key])
+        if i not in string.ascii_letters:
+            b.append(i)
         if i in a:
             b.append(a[a.index(i) + key])
         if i in u:
