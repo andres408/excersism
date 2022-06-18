c8c2f9e2ea6e4a3db0f14a4e39b851fe08991ff5
Kali <kali@kali>
2022-06-17 20:25:29 -0400

    Add rotational_cipher.py file

---
+++ rotational_cipher.py
@@ -0,0 +1,22 @@
+import string
+a = (string.ascii_lowercase*2)
+u = (string.ascii_uppercase*5)
+d = ('945330152468'*5)
+p = ('<>,.-_:;/&%$#\"\'?¿=*+-!' * 5)
+b = []
+def rot(text,key):
+    for i in text:
+        if i in p:
+            b.append(p[p.index(i) + key])
+        if i in string.digits:
+            b.append(d[d.index(i) + key])
+        if i in a:
+            b.append(a[a.index(i) + key])
+        if i in u:
+            b.append(u[u.index(i) + key])
+    return ''.join(b)
+    
+if __name__=='__main__':
+    text=str(input('Ingrese text\n'))
+    key = int(input('Ingrese key\n'))
+    print(rot(text,key))
