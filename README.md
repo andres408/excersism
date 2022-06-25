c26cb7ec03c49c1500634c9bab0329962884e900
Kali <kali@kali>
2022-06-25 16:23:22 -0400

    Addition atbash_cipher.py file and readme file  update

--- README.md
+++ README.md
@@ -350,4 +350,25 @@ The outer circle has a radius of 10 units (this is equivalent to the total radiu
 
 Write a function that given a point in the target (defined by its Cartesian coordinates x and y, where x and y are real), returns the correct amount earned by a dart landing at that point.
 
+ATBASH_CIPHER.PY
+Instructions
+Create an implementation of the atbash cipher, an ancient encryption system created in the Middle East.
+
+The Atbash cipher is a simple substitution cipher that relies on transposing all the letters in the alphabet such that the resulting alphabet is backwards. The first letter is replaced with the last letter, the second with the second-last, and so on.
+
+An Atbash cipher for the Latin alphabet would be as follows:
+
+Plain:  abcdefghijklmnopqrstuvwxyz
+Cipher: zyxwvutsrqponmlkjihgfedcba
+It is a very weak cipher because it only has one possible key, and it is a simple mono-alphabetic substitution cipher. However, this may not have been an issue in the cipher's time.
+
+Ciphertext is written out in groups of fixed length, the traditional group size being 5 letters, leaving numbers unchanged, and punctuation is excluded. This is to make it harder to guess things based on word boundaries.
+
+Examples
+Encoding test gives gvhg
+Encoding x123 yes gives c123b vh
+Decoding gvhg gives test
+Decoding gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt gives thequickbrownfoxjumpsoverthelazydog
+
+
 Andres Felipe
--- atbash_cipher.py
+++ atbash_cipher.py
@@ -1,18 +1,40 @@
+'''Program to encode and decode a text'''
+import string
 voc=[ chr(i) for i in range(97,123)]
 v = voc[::-1]
 def encode(plain_text):
+    '''Param: str - A plain text with numbers and symbols
+       return: str - A encoded text changing the words for the word + 13 position in the alphabet, numbers stay equal. spaces and symbols are erasen and each 5 digits one space.'''
     encoded_text=''
+    h = 0
     for c in plain_text.lower():
-        if c not in v:
+        if c.isspace() and c in string.punctuation:
+            encoded_text += ''
+        if c.isnumeric():
             encoded_text += c
+            h +=1
         if c in v:
-            encoded_text += voc[v.index(c)]   
-    return encoded_text
+            encoded_text += voc[v.index(c)]
+            h+=1
+        if h==5:
+            encoded_text +=' '
+            h=0    
+    return encoded_text.strip()
 def decode(ciphered_text):
+    '''Param: str - A encoded text
+       return: str - A plain text without spaces'''
     decoded_text=''
     for chr in ciphered_text.lower():
-        if chr not in v:
+        if chr.isspace() and chr in string.punctuation:
+            decoded_text +=''
+        if chr.isnumeric():
             decoded_text += chr
         if chr in v:
             decoded_text += v[voc.index(chr)]
     return decoded_text
+    
+if __name__=='__main__':
+    plain_text=str(input('Ingresa palabra\n'))
+    print(encode(plain_text))
+    ciphered_text=str(input('Ingresa texto\n'))
+    print(decode(ciphered_text))
