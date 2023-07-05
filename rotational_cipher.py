052714b11409e3c835c44e05c308835ad7826cb1
Andres <a---f@outlook.com>
2023-07-04 22:20:02 -0500

    Actualizacion rotational cipher

--- rotational_cipher.py
+++ rotational_cipher.py
@@ -5,12 +5,10 @@ def rot(text,key):
     for i in text:
         if i in a:
             b.append(a[a.index(i) + key])
-            b.append(a[a.index(i) + 2 + key])
-        
+            b.append(a[a.index(i) + 2 + key])       
     return ''.join(b)
 
-
 if __name__=='__main__':
-    text=str(input('Ingrese text\n'))
+    text=input('Ingrese text\n')
     key = int(input('Ingrese key\n'))
     print(rot(text,key))
