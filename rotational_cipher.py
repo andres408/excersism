4e1453e1593e2f86be9fb1ffea5a1ee82853d32d
Andres <a---f@outlook.com>
2023-11-02 15:43:34 -0500

    rotational_cipher.py cambios

--- rotational_cipher.py
+++ rotational_cipher.py
@@ -1,14 +1,25 @@
-import string
-a = (string.printable*2)
+a = "H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\H'-1W~D9`U?lrk/2u{=3Icnzp*&)ZbRj}iF>8em[]70BfA4\"OC5%!N,_Qqh6g^;.$VTG(YysMKX:<@EoJPw#Latx+vdS\|\\\\\\\\"
 b = []
-def rot(text,key):
+def codificar(text,key):
     for i in text:
         if i in a:
             b.append(a[a.index(i) + key])
             b.append(a[a.index(i) + 2 + key])       
     return ''.join(b)
 
+def descodificar(text, key):
+    for i in range(0,len(text),2):
+        if text[i] in a:
+            if descodificar:
+                b.append(a[a.index(text[i]) - key])
+    return ''.join(b)
+
 if __name__=='__main__':
+    funcion=input('Ingrese la funcion requerida codificar o descodificar\n (c,d)')
     text=input('Ingrese text\n')
     key = int(input('Ingrese key\n'))
-    print(rot(text,key))
+    if funcion == 'c':
+        print(codificar(text,key))
+    elif funcion == 'd':
+        print(descodificar(text,key))
+    else:print('Ingrese una opcion valida')
