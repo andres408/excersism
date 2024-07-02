2e67d3c59f1cd5be932fb513e3ad8f5ebf5841dc
af <af@debian>
2024-07-02 15:56:17 -0500

    add resistor_color.py file

--- resistor_color.py
+++ resistor_color.py
@@ -1,16 +1,18 @@
 '''Program to return the values of the color bands and all the color bands of the resistors'''
+
 def color_code(color):
+    color_bands = ['black','brown','red','orange','yellow',
+               'green','blue','violet','grey','white']
     '''Param: str - Color band introduced by user
        Return: int - Numerical value associated with the color band introduced'''
-    return colors().index(color)
+    colors =[]
+    color_list = [x.strip() for x in color.split(' ')]
+    for i in color_list:
+       colors.append(color_bands.index(i))
 
-
-def colors():
-    '''Return : list - different color bands'''
-    color_bands = ['Black','brown','red','orange','yellow',
-    'green','blue','violet','grey','white']
-    return color_bands
-
+    return ''.join(str(x) for x in colors[:2])
+   
 if __name__=='__main__':
     color = str(input('Enter a color band\n'))
     print(color_code(color))
+
